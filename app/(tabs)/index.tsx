fcd20dd6884607504722112d1ee32af689670d66
Mikolaj <rudkowski.dev@gmail.com>
2025-11-16 14:06:26 +0100

    add Flexbox

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,21 @@
 import DateDisplay from '@/components/Zad_1/date-display';
 import MapImage from '@/components/Zad_2/map-image';
 import StyledText from '@/components/Zad_3/styled-text';
+import FlexboxLayout from '@/components/Zad_4/flexbox-layout';
 import { View, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={{ padding: 24 }}>
+    <View style={{ flex: 1, padding: 24 }}>
       <Text>Zad. 1</Text>
       <DateDisplay />
       <Text>Zad. 2</Text>
       <MapImage />
       <Text>Zad. 3</Text>
       <StyledText />
+      <Text>Zad. 4</Text>
+      <FlexboxLayout />
+      <Text>Zad. 5</Text>
     </View>
   );
 }
---
+++ components/Zad_4/flexbox-layout.tsx
@@ -0,0 +1,39 @@
+import { View, StyleSheet } from 'react-native';
+
+export default function FlexboxLayout() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.blueBox} />
+      <View style={styles.redBox} />
+      <View style={styles.bottomRow}>
+        <View style={styles.yellowBox} />
+        <View style={styles.yellowBox} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  blueBox: {
+    flex: 1,
+    backgroundColor: 'blue',
+    marginBottom: 4,
+  },
+  redBox: {
+    flex: 2,
+    backgroundColor: 'red',
+    marginBottom: 4,
+  },
+  bottomRow: {
+    flex: 1,
+    flexDirection: 'row',
+    columnGap: 4,
+  },
+  yellowBox: {
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+});
